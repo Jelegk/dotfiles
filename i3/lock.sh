0b34159bbd5229002e59b4a48f857266200aa3bb
Jelegk <125297114+Jelegk@users.noreply.github.com>
2024-10-01 02:13:02 -0400

    Quick "&" fix of xcompmgr in lock.sh

--- i3/lock.sh
+++ i3/lock.sh
@@ -7,7 +7,7 @@ checklock() {
     fi
 }
 # Turn on windows crossfade
-xcompmgr -fF
+xcompmgr -fF &
 # check each minutes for inactivity, if so return to suspend mode
 checklock &
 # Lock screen 9note: keep bar-step value high enough to negate the pixelated "pyramid" from appearing)
@@ -16,7 +16,7 @@ i3lock -n -i ~/.config/wallpapers/copland_os_blue.png -F -e \
         --greeter-color=212121ff --greeter-font=D050000L --greeter-align=1 --greeter-size=24 \
         --bar-color=00000000 --ringver-color=096b33ff --ringwrong-color=6b0b10ff --bar-step=20 \
         --bar-pos="x+0:h" --bar-total-width=w --bar-base-width=8 --bar-max-height=12 --bar-count=20 \
-        --bar-direction=1 --verif-text="" --wrong-text="" -noinput-text="" --no-modkey-text --pass-media-keys &&
+        --bar-direction=1 --verif-text="" --wrong-text="" --noinput-text="" --no-modkey-text --pass-media-keys &&
 #        --inside-color=00000000 --ring-color=bababaff --line-uses-inside \
 #        --keyhl-color=5c5d66ff --bshl-color=571b21ff --separator-color=00000000 \
 #        --insidever-color=096b33ff --insidewrong-color=6b0b10ff \
