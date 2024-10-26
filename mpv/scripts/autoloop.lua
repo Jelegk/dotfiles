42d510661c4f3b238d8f57e5fe38f22b4df9ca49
Jelegk <125297114+Jelegk@users.noreply.github.com>
2024-10-25 21:45:36 -0400

    mpv scripts (2/2)

---
+++ mpv/scripts/autoloop.lua
@@ -0,0 +1,38 @@
+-- mpv issue 5222
+-- Automatically set loop-file=inf for duration <= given length. Default is 5s
+-- Use autoloop_duration=n in script-opts/autoloop.conf to set your preferred length
+-- Alternatively use script-opts=autoloop-autoloop_duration=n in mpv.conf (takes priority)
+-- Also disables the save-position-on-quit for this file, if it qualifies for looping.
+
+
+require 'mp.options'
+
+function getOption()
+    -- Use recommended way to get options
+    local options = {autoloop_duration = 1200}
+    read_options(options)
+    autoloop_duration = options.autoloop_duration
+end
+
+function set_loop()
+    local duration = mp.get_property_native("duration")
+
+    -- Checks whether the loop status was changed for the last file
+    was_loop = mp.get_property_native("loop-file")
+
+    -- Cancel operation if there is no file duration
+    if not duration then
+        return
+    end
+
+    -- Loops file if was_loop is false, and file meets requirements
+    if not was_loop and duration <= autoloop_duration then
+        mp.set_property_native("loop-file", true)
+        -- Unloops file if was_loop is true, and file does not meet requirements
+    elseif was_loop and duration > autoloop_duration then
+        mp.set_property_native("loop-file", false)
+    end
+end
+
+getOption()
+mp.register_event("file-loaded", set_loop)
