2ff3589e200cc4e9a822c0347e5c2bea6b5c3f53
Jelegk <125297114+Jelegk@users.noreply.github.com>
2024-11-25 20:29:14 -0500

    mpv scripts (numpad seeks and slimbar update/2)

--- mpv/scripts-if-AV/slim-progressbar.lua
+++ mpv/scripts-if-AV/slim-progressbar.lua
@@ -1,7 +1,8 @@
 -- A bare bone "progressbar.lua" when pausing:
 
 -- config
-local STAY_TIMER = 0 -- 0 and less to disable, aka stay on screen until playback starts again
+local SEEK_TIMER = 0.2 -- must be > 0
+local PAUSE_TIMER = 0 -- 0 and less to disable, aka stay on screen until playback starts again
 local COLOR_PROGRESS = "48184F" -- BBGGRR
 local COLOR_BACKGROUND = "181818"
 local OPACITY = "0" -- [0, 255] -- not so sure about these numbers anymore, but whatever: 0 = opaque
@@ -23,28 +24,25 @@ local function erase_progressbar()
     background_bar:remove()
 end
 
-local function render_progressbar()
+local function render_progressbar(timer)
     local progress_w = math.floor(px_to_osd_scale(vid_w) * 0.01 * mp.get_property_number('percent-pos', 0))
     local background_w = px_to_osd_scale(vid_w + 1) - progress_w -- +1 to ensure that it reaches the right border (sometimes, it's a bit short)
 
     -- See http://www.tcax.org/docs/ass-specs.htm for strings components
-    local progress = string.format([[{\p1\bord0\alpha&H%s\1c&H%s&\pos(%d,%d)}]], 
-                     OPACITY, COLOR_PROGRESS, vid_margin_x, OSD_INTERNAL_H) ..
-                     string.format([[m 0 0 l %d 0 %d %d 0 %d]], 
-                     progress_w, progress_w, -BAR_HEIGHT, -BAR_HEIGHT)
+    progress_bar.data = string.format([[{\p1\bord0\alpha&H%s\1c&H%s&\pos(%d,%d)}]], 
+                        OPACITY, COLOR_PROGRESS, vid_margin_x, OSD_INTERNAL_H) ..
+                        string.format([[m 0 0 l %d 0 %d %d 0 %d]], 
+                        progress_w, progress_w, -BAR_HEIGHT, -BAR_HEIGHT)
 
-    local background = string.format([[{\p1\bord0\alpha&H%s\1c&H%s&\pos(%d,%d)}]], 
-                       OPACITY, COLOR_BACKGROUND, vid_margin_x + progress_w, OSD_INTERNAL_H) ..
-                       string.format([[m 0 0 l %d 0 %d %d 0 %d]], 
-                       background_w, background_w, -BAR_HEIGHT, -BAR_HEIGHT)
-
-    progress_bar.data = progress
-    background_bar.data = background
+    background_bar.data = string.format([[{\p1\bord0\alpha&H%s\1c&H%s&\pos(%d,%d)}]], 
+                          OPACITY, COLOR_BACKGROUND, vid_margin_x + progress_w, OSD_INTERNAL_H) ..
+                          string.format([[m 0 0 l %d 0 %d %d 0 %d]], 
+                          background_w, background_w, -BAR_HEIGHT, -BAR_HEIGHT)
 
     progress_bar:update()
     background_bar:update()
 
-    if STAY_TIMER > 0 then mp.add_timeout(STAY_TIMER, erase_progressbar) end
+    if timer > 0 then mp.add_timeout(timer, erase_progressbar) end
 end
 
 local function resize_progressbar()
@@ -55,10 +53,9 @@ local function resize_progressbar()
     --alternatively:  vid_margin_x = px_to_osd_scale(math.ceil((win_w - vid_w) * 0.5))
 end
 
-local function osc_not_hovered()
+local function osc_not_hovered()  -- 100 24
     local _, m_pos_y = mp.get_mouse_pos()
-    m_pos_y = m_pos_y / win_h
-    return m_pos_y < 0.9 and m_pos_y > 0.025
+    return m_pos_y < win_h - 100 and m_pos_y > 24
 end
 
 local paused = false
@@ -66,12 +63,13 @@ local osc_hidden = true
 
 mp.observe_property("pause", "bool", function(_, pause_prop)
     paused = pause_prop
-    if paused and osc_not_hovered() then render_progressbar()
+    if paused and osc_not_hovered() then render_progressbar(PAUSE_TIMER)
     else erase_progressbar() end
 end)
 mp.register_event('file-loaded', function() if paused then erase_progressbar() end end)
-mp.register_event("seek", function() if paused and osc_not_hovered() then render_progressbar() end end)
 mp.observe_property("mouse-pos", "native", function() if paused then erase_progressbar() end end)
+mp.register_event("seek", function() if osc_not_hovered() then render_progressbar(SEEK_TIMER) end end)
+mp.add_forced_key_binding(nil, "slimbar_on_seek", function() if osc_not_hovered() then render_progressbar(SEEK_TIMER) end end)
 
 mp.observe_property("osd-dimensions", "native", resize_progressbar)
 
