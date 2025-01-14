d5fd83cec12e75cb56916c5e4531ba8f5d37251f
Jelegk <legault.jeremy18@gmail.com>
2025-01-14 00:48:26 -0500

    Sync with main computer

--- .config/polybar/launch_polybar.sh
+++
@@ -1,5 +0,0 @@
-# Terminate already running bar instances
-pkill '^polybar$'
-
-polybar top 2>&1 | tee -a /tmp/polybar_top_bar.log & disown
-polybar bottom 2>&1 | tee -a /tmp/polybar_bottom_bar.log & disown
